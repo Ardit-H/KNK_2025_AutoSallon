c6abdc8921122c2a5a801ff91641f33cbcba7042
ElsaKrasniqii <elsa.krasniqi12@student.uni-pr.edu>
2025-03-22 21:19:35 +0100

    init commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.knk_2025_autosallon {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.example.knk_2025_autosallon to javafx.fxml;
     exports com.example.knk_2025_autosallon;
 }
