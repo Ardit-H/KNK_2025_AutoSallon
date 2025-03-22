8357c924395e93209db0bf4475b86bf926bf0a1f
Ardit-H <ardit.hyseni5@student.uni-pr.edu>
2025-03-22 13:56:36 +0100

    init commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.knk_2025_autosallon {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.knk_2025_autosallon to javafx.fxml;
+    exports com.example.knk_2025_autosallon;
+}
