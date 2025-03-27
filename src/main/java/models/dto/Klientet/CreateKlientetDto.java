b890aaba2ffafbb662eeb28a9f6da74786f4ff9b
Ardit-H <ardit.hyseni5@student.uni-pr.edu>
2025-03-27 15:57:00 +0100

    Krijimi i klases CreateKlientetDto

--- src/main/java/models/dto/Klientet/CreateKlientetDto.java
+++ src/main/java/models/dto/Klientet/CreateKlientetDto.java
@@ -1,4 +1,67 @@
 package models.dto.Klientet;
 
 public class CreateKlientetDto {
+    private String emri;
+    private String mbiemri;
+    private String email;
+    private String nrtelefonit;
+    private String adresa;
+    private String data_regjistrimit;
+
+    public CreateKlientetDto(String emri,String mbiemri,String email,String nrtelefonit,String adresa,String data_regjistrimit){
+      this.emri=emri;
+      this.mbiemri=mbiemri;
+      this.email=email;
+      this.nrtelefonit=nrtelefonit;
+      this.adresa=adresa;
+      this.data_regjistrimit=data_regjistrimit;
+    }
+
+    public String getEmri() {
+        return emri;
+    }
+
+    public String getMbiemri() {
+        return mbiemri;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNrtelefonit() {
+        return nrtelefonit;
+    }
+
+    public String getAdresa() {
+        return adresa;
+    }
+
+    public String getData_regjistrimit() {
+        return data_regjistrimit;
+    }
+
+    public void setEmri(String emri) {
+        this.emri = emri;
+    }
+
+    public void setMbiemri(String mbiemri) {
+        this.mbiemri = mbiemri;
+    }
+
+    public void setNrtelefonit(String nrtelefonit) {
+        this.nrtelefonit = nrtelefonit;
+    }
+
+    public void setAdresa(String adresa) {
+        this.adresa = adresa;
+    }
+
+    public void setData_regjistrimit(String data_regjistrimit) {
+        this.data_regjistrimit = data_regjistrimit;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
