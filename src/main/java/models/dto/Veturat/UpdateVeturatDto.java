f1a31a83747e99d4b01e488ddaa0dc617cb0d184
ElsaKrasniqii <elsa.krasniqi12@student.uni-pr.edu>
2025-04-01 14:36:26 +0200

    Krijimi i klasees CreateVeturatDto

--- src/main/java/models/dto/Veturat/UpdateVeturatDto.java
+++ src/main/java/models/dto/Veturat/UpdateVeturatDto.java
@@ -1,4 +1,12 @@
 package models.dto.Veturat;
 
 public class UpdateVeturatDto {
+    private int vetura_id;
+
+    public UpdateVeturatDto(int vetura_id){
+        this.vetura_id = vetura_id;
+    }
+    public int getVetura_id(){
+        return vetura_id;
+    }
 }
