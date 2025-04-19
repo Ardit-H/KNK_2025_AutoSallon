9c64b66729290f6b973863cbf6d7f166c1348840
Ardit-H <ardit.hyseni5@student.uni-pr.edu>
2025-04-19 16:26:04 +0200

    Kriji i klases KlientetService

---
+++ src/main/java/Test/KlientetServiceTest.java
@@ -0,0 +1,42 @@
+package Test;
+
+import models.dto.Klientet.CreateKlientetDto;
+import models.dto.Klientet.Klientet;
+import models.dto.Klientet.UpdateKlientiDto;
+import repository.KlientetRepository;
+import services.KlientetService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KlientetServiceTest {
+    public static void main(String[] args){
+        KlientetService klientetService=new KlientetService();
+        try{
+           Klientet klientet= klientetService.getById(6);
+          System.out.println( klientet.getEmri());
+//         List<Klientet> klients=klientetService.getAll();
+//         for(int i=0;i< klients.size();i++){
+//             Klientet klient = klients.get(i);
+//             System.out.println("ID: " + klient.getKid());
+//             System.out.println("Emri: " + klient.getEmri());
+//             System.out.println("Mbiemri: " + klient.getMbiemri());
+//             System.out.println("Email: " + klient.getEmail());
+//             System.out.println("Nr Telefonit: " + klient.getNrtelefonit());
+//             System.out.println("Adresa: " + klient.getAdresa());
+//             System.out.println("Data Regjistrimit: " + klient.getData_regjistrimit());
+//             System.out.println("-----------------------------");
+//         }
+//            CreateKlientetDto create=new CreateKlientetDto("Fisnik","Beqiri","fis@gmail.com","+49824948","Peje-Rruga Shekspiri");
+//          klientetService.create(create);
+//            UpdateKlientiDto update=new UpdateKlientiDto();
+//            update.setId(8);
+//            update.setEmail("fisnik2@gmail.com");
+//            update.setAdresa("Ferizaj");
+//            update.setNrtelefonit("049586724");
+//          klientetService.update(update);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
