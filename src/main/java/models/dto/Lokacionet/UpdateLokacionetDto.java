3154a3d64861092dcbb953cc2af68344e3bb790b
Erzana Beqaj <erzana.beqaj@student.uni-pr.edu>
2025-04-28 20:24:40 +0200

    Krijimi i klases Lokacionet.java

---
+++ src/main/java/models/dto/Lokacionet/UpdateLokacionetDto.java
@@ -0,0 +1,34 @@
+package models.dto.Lokacionet;
+
+public class UpdateLokacionetDto {
+    private int id;
+    private String emri;
+    private String adresa;
+    private String qyteti;
+    private String nrtelefonit;
+
+    public UpdateLokacionetDto(){}
+
+    public int getId (){ return id; }
+
+    public void setId( int id){ this.id = id; }
+
+    public String getEmri(){ return emri; }
+
+    public void setEmri(String emri){ this.emri = emri; }
+
+    public String getAdresa(){ return adresa; }
+
+    public void setAdresa(String adresa){ this.adresa = adresa; }
+
+    public String getQyteti(){ return qyteti; }
+
+    public void setQyteti(String qyteti){ this.qyteti = qyteti; }
+
+    public String getNrtelefonit(){ return nrtelefonit; }
+
+    public void setNrtelefonit( String nrtelefonit){ this.nrtelefonit = nrtelefonit; }
+
+
+
+}
