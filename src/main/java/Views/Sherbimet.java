b02e572956cd2cc69f94b50bac1e12e27d14f932
Ardit-H <ardit.hyseni5@student.uni-pr.edu>
2025-05-01 22:21:29 +0200

    Shtimi i butonave per ndryshimin e gjuhes tek sherbimet.fxml dhe rregullimi i tyre

--- src/main/java/Views/Sherbimet.java
+++ src/main/java/Views/Sherbimet.java
@@ -2,15 +2,24 @@ package Views;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import services.LanguageManager;
+import services.SceneManager;
+import utils.SceneLocator;
 
 public class Sherbimet extends Application {
+    public static Scene scene;
     public void start(Stage stage) throws Exception{
         FXMLLoader fxmlLoader=new FXMLLoader(
-                Sherbimet.class.getResource("/Views/sherbimet.fxml")
+                getClass().getResource(SceneLocator.SHERBIMET)
         );
-        Scene scene=new Scene(fxmlLoader.load(),700,500);
+        LanguageManager languageManager=LanguageManager.getInstance();
+        fxmlLoader.setResources(languageManager.getResourceBundle());
+        Parent parent=fxmlLoader.load();
+        scene=new Scene(parent);
+        SceneManager.initialize(scene,SceneLocator.SHERBIMET);
         stage.setScene(scene);
         stage.show();
     }
