cfcd5c7e21aa6ce206b0b8023a634a67efc18137
Ardit-H <ardit.hyseni5@student.uni-pr.edu>
2025-05-05 02:31:30 +0200

    Rregullim i dizajnit te admin_dashboard.fxml

--- src/main/java/controllers/Admin_DashboardController.java
+++ src/main/java/controllers/Admin_DashboardController.java
@@ -2,15 +2,16 @@ package controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import services.SceneManager;
 import utils.SceneLocator;
 
 public class Admin_DashboardController {
     @FXML private Button btn_klientet;
-    @FXML private VBox centerVBox;
+    @FXML private AnchorPane centerPane;
 
    @FXML private void handleLoadKlientet()throws Exception{
-        SceneManager.load(SceneLocator.KLIENTET,centerVBox);
+        SceneManager.load(SceneLocator.KLIENTET,centerPane);
     }
 }
