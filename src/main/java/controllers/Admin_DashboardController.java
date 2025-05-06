21931a0b4935bd9697642e17d588a8bbc3617d7d
Ardit-H <ardit.hyseni5@student.uni-pr.edu>
2025-05-06 12:51:24 +0200

    Shtimi dhe rregullimi i butonit per nderrimin e gjuhes te admin_dashboard.fxml dhe Admin_DashboardController.java

--- src/main/java/controllers/Admin_DashboardController.java
+++ src/main/java/controllers/Admin_DashboardController.java
@@ -3,24 +3,39 @@ package controllers;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.util.Duration;
+import services.LanguageManager;
 import services.SceneManager;
 import utils.SceneLocator;
 
+import java.util.Locale;
+
 public class Admin_DashboardController {
-    @FXML private Button btn_klientet;
     @FXML private AnchorPane centerPane;
     @FXML private VBox sideMenu;
     @FXML private Button menuToggleButton;
     private boolean menuVisible = false;
-
+    @FXML private Button languageToggleButton;
+    private boolean isEnglish = true;
+    private LanguageManager languageManager;
+    public Admin_DashboardController(){
+        this.languageManager=LanguageManager.getInstance();
+    }
     @FXML public void initialize() {
         sideMenu.setTranslateX(-200);
         sideMenu.setVisible(false);
         sideMenu.setManaged(false);
         menuToggleButton.setOnAction(event -> toggleMenu());
+        if (languageManager.getLocale().equals(new Locale("en"))) {
+            setLanguageIcon("/Images/language-en.png");
+            isEnglish = true;
+        } else {
+            setLanguageIcon("/Images/language-sq.png");
+            isEnglish = false;
+        }
     }
 
     private void toggleMenu() {
@@ -39,12 +54,34 @@ public class Admin_DashboardController {
             });
         }
         transition.play();
-        menuVisible = !menuVisible;
+        menuVisible =!menuVisible;
     }
    @FXML private void handleLoadKlientet()throws Exception{
         SceneManager.load(SceneLocator.KLIENTET,centerPane);
     }
     @FXML private void handleLoadDashboard_Home()throws Exception{
         SceneManager.load(SceneLocator.DASHBOARD_HOME,centerPane);
+    }
+
+@FXML private void handleLanguageToggle() throws Exception {
+    if (isEnglish) {
+        loadLanguage(new Locale("sq"));
+        setLanguageIcon("/Images/language-sq.png");
+    } else {
+        loadLanguage(Locale.ENGLISH);
+        setLanguageIcon("/Images/language-en.png");
+    }
+    isEnglish =!isEnglish;
+}
+
+    private void setLanguageIcon(String imagePath) {
+        ImageView imageView = new ImageView(new javafx.scene.image.Image(getClass().getResourceAsStream(imagePath)));
+        imageView.setFitHeight(20);
+        imageView.setFitWidth(30);
+        languageToggleButton.setGraphic(imageView);
+    }
+    private void loadLanguage(Locale locale) throws Exception{
+        languageManager.setLocale(locale);
+        SceneManager.reload();
     }
 }
