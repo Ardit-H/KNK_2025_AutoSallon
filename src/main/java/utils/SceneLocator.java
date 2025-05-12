e7e3f96f803dec26a3facb1862a8cb8498fc5484
Dua-Gashi <dua.gashi@student.uni-pr.edu>
2025-05-12 18:33:24 +0200

    Perkthimi

--- src/main/java/utils/SceneLocator.java
+++ src/main/java/utils/SceneLocator.java
@@ -17,4 +17,5 @@ public class SceneLocator {
     public static final String VETURAT="/Views/veturat.fxml";
     public static final String TESTDRIVES="/Views/testDrives.fxml";
     public static final String GARANCIA="/Views/garancia.fxml";
+    public static final String FATURAT = "/Views/faturat.fxml";
 }
