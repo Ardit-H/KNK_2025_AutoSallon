5cb24192dac0053ee88b7b2d92c7a1a60a65698b
Dua-Gashi <dua.gashi@student.uni-pr.edu>
2025-05-12 17:58:53 +0200

    Krijimi i FaturatController dhe faturat.fxml

--- src/main/java/models/dto/Faturat/CreateFaturatDto.java
+++ src/main/java/models/dto/Faturat/CreateFaturatDto.java
@@ -8,7 +8,7 @@ public class CreateFaturatDto {
         private Double shumaTotale;
         private String llojiPageses;
 
-        public CreateFaturatDto() {
+        public CreateFaturatDto(int shitjeId, String dataFatures, Double shumaTotale, String llojiPageses) {
             this.shitjeId = shitjeId;
             this.dataFatures = dataFatures;
             this.shumaTotale = shumaTotale;
--- src/main/java/utils/SceneLocator.java
+++ src/main/java/utils/SceneLocator.java
@@ -17,4 +17,7 @@ public class SceneLocator {
     public static final String VETURAT="/Views/veturat.fxml";
     public static final String TESTDRIVES="/Views/testDrives.fxml";
     public static final String GARANCIA="/Views/garancia.fxml";
+    public static final String FATURAT="/Views/faturat.fxml";
+    public static final String RIPARIMET="/Views/riparimet.fxml";
+    public static final String PARTNERET="/Views/partneret.fxml";
 }
