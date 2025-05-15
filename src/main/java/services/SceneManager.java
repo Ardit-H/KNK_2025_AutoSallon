173269d82a2d5ee2e206d576c2a18abcd3f793cf
Dua-Gashi <dua.gashi@student.uni-pr.edu>
2025-05-15 16:25:15 +0200

    Ndryshimi te perkthimi

--- src/main/java/services/SceneManager.java
+++ src/main/java/services/SceneManager.java
@@ -19,7 +19,7 @@ public class SceneManager {
 
     private SceneManager(){
         this.languageManager = LanguageManager.getInstance();
-        this.currentPath = SceneLocator.SHITJET;
+        this.currentPath = SceneLocator.FATURAT;
         this.scene = this.initScene();
     }
 
